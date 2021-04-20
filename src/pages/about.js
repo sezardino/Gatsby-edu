480507e6c397fca94687f777cd01fc05f74a2e5d
Edward <edward.arechwa@gogomedia.pl>
2021-04-20 13:50:07 +0200

    remaster: remaster pages templates

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,20 +2,19 @@ import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 
-const HomePage = ({ data }) => {
-  console.log(data);
+const About = ({ data }) => {
   const fields = data.markdownRemark.frontmatter;
   return (
     <Layout title={fields.title}>
       <h1>{fields.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
+      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
     </Layout>
   );
 };
 
 const query = graphql`
 query {
-  markdownRemark(fileAbsolutePath: {regex: "/home/"}) {
+  markdownRemark(fileAbsolutePath: {regex: "/about/"}) {
     frontmatter {
       title
     }
@@ -25,4 +24,4 @@ query {
 `;
 
 export { query };
-export default HomePage;
+export default About;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,20 +2,19 @@ import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 
-const HomePage = ({ data }) => {
-  console.log(data);
+const Contact = ({ data }) => {
   const fields = data.markdownRemark.frontmatter;
   return (
     <Layout title={fields.title}>
       <h1>{fields.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
+      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
     </Layout>
   );
 };
 
 const query = graphql`
 query {
-  markdownRemark(fileAbsolutePath: {regex: "/home/"}) {
+  markdownRemark(fileAbsolutePath: {regex: "/contact/"}) {
     frontmatter {
       title
     }
@@ -25,4 +24,4 @@ query {
 `;
 
 export { query };
-export default HomePage;
+export default Contact;
