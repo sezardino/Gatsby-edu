94d00f6c1e7fe20c3f27ee1d10aefddae3a972d2
Edward <edward.arechwa@gogomedia.pl>
2021-04-22 12:37:31 +0200

    feat: blog page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,44 @@
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 
 const Contact = ({ data }) => {
   const fields = data.markdownRemark.frontmatter;
+  const contactData = fields.contactData;
+  console.log(contactData);
   return (
     <Layout title={fields.title}>
       <h1>{fields.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
+      <div>
+        <p>mail: {contactData.mail}</p>
+        <p>tel: {contactData.tel}</p>
+        <ul style={{display: 'flex', flexDirection: 'column'}}>
+          {contactData.social.map((item) => {
+            return <Link to={item.link} key={item.name}>{item.label}: {item.link}</Link>
+          })}
+        </ul>
+      </div>
     </Layout>
   );
 };
 
 const query = graphql`
-query {
-  markdownRemark(fileAbsolutePath: {regex: "/contact/"}) {
-    frontmatter {
-      title
+  {
+    markdownRemark(fileAbsolutePath: { regex: "/contact/" }) {
+      frontmatter {
+        title
+        contactData {
+          mail
+          tel
+          social {
+            label
+            link
+            name
+          }
+        }
+      }
     }
-    html
   }
-}
 `;
 
 export { query };
