25b29b64a78bec5d08c86564c3f693adeabc4269
Edward <edward.arechwa@gogomedia.pl>
2021-04-22 10:02:04 +0200

    feat: add link for workCard

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,4 +1,4 @@
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
 import { GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
@@ -6,26 +6,26 @@ import Layout from "../components/layout";
 
 const WorkCard = (data) => {
   const fields = data.node.childMarkdownRemark.frontmatter;
-  const image = getImage(fields.image)
-  return <div key={data.node.childMarkdownRemark.id}>
-    <h3>{fields.title}</h3>
-    <GatsbyImage image={image} alt={fields.title}/>
-  </div>;
+  const image = getImage(fields.image);
+  return (
+    <Link to={data.node.childMarkdownRemark.fields.slug} key={data.node.childMarkdownRemark.id}>
+      <h3>{fields.title}</h3>
+      <GatsbyImage image={image} alt={fields.title} />
+    </Link>
+  );
 };
 
 const Works = ({ data }) => {
   const fields = data.markdownRemark.frontmatter;
   const works = data.allFile.edges;
-  console.log(data.allFile.edges);
   const workCards = works.map(WorkCard);
-  console.log(workCards);
   return (
     <Layout title={fields.title}>
       <h1>{fields.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}></div>
       <ul>
         {workCards.map((item, index) => {
-          return <li key={index}>{item}</li>
+          return <li key={index}>{item}</li>;
         })}
       </ul>
     </Layout>
@@ -33,12 +33,12 @@ const Works = ({ data }) => {
 };
 
 const query = graphql`
-  query {
+  query MyQuery {
     markdownRemark(fileAbsolutePath: { regex: "/works/" }) {
+      html
       frontmatter {
         title
       }
-      html
     }
     allFile(
       filter: {
@@ -50,11 +50,18 @@ const query = graphql`
         node {
           childMarkdownRemark {
             id
+            fields {
+              slug
+            }
             frontmatter {
               title
               image {
                 childImageSharp {
-                  gatsbyImageData(width: 250, layout: FIXED, placeholder: BLURRED)
+                  gatsbyImageData(
+                    width: 250
+                    layout: FIXED
+                    placeholder: BLURRED
+                  )
                 }
               }
             }
