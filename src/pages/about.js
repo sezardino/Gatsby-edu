c653ee1757519f45840248e228abde17e6d8d9f4
Edward <edward.arechwa@gogomedia.pl>
2021-04-26 15:32:09 +0200

    feat: add fields to about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,17 +2,13 @@ import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../components/layout";
 
-const changeP = (string, className) => {
-  return string.replace('<p>', `<p class="${className}">`)
-}
-
 const About = ({ data }) => {
   const fields = data.markdownRemark.frontmatter;
-  const html = changeP(data.markdownRemark.html, 'some-class');
   return (
     <Layout title={fields.title}>
       <h1>{fields.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      <p>{fields.frstText}</p>
+      <p>{fields.scndText}</p>
     </Layout>
   );
 };
@@ -22,8 +18,9 @@ query {
   markdownRemark(fileAbsolutePath: {regex: "/about/"}) {
     frontmatter {
       title
+      frstText
+      scndText
     }
-    html
   }
 }
 `;
